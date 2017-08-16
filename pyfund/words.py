7c5ab6747cb73621d168bf7e41e50a9b6658472b
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-16 11:32:35 +0200

    sending args in command line to a module

--- pyfund/words.py
+++ pyfund/words.py
@@ -1,7 +1,8 @@
+import sys
 from urllib.request import urlopen
 
 
-def fetch_words():
+def fetch_words(url):
     with urlopen('http://sixty-north.com/c/t.txt') as story:
             story_words = []
             for line in story:
@@ -11,21 +12,25 @@ def fetch_words():
     return story_words
 
 
-def print_words(story_words):
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
 
 # print(__name__)  # To print the name of python file
 
+
 def main():
-    words = fetch_words()
-    print(words)
+    url = sys.argv[1]
+    words = fetch_words(url)
+    print_items(words)
 
 
 if __name__ == '__main__':
     main()
 
-# to execute this, python words.py in REPL after doing import words
+# to execute multiple functions in a module, in REPL, from words import (fetch_words, print_words)
+# print_words(fetch_words())
+# from words import (fetch_words, print_words, main)
 
 
 
