e48676a44ec03613fe9a93cebecaec6933e1d11b
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-16 11:18:41 +0200

    dividing module into number of functions

--- pyfund/words.py
+++ pyfund/words.py
@@ -8,13 +8,22 @@ def fetch_words():
                 line_words = line.decode('utf-8').split() # to convert bytes of data from http response to string
                 for word in line_words:
                     story_words.append(word)
+    return story_words
+
+
+def print_words(story_words):
     for word in story_words:
         print(word)
 
 # print(__name__)  # To print the name of python file
 
+def main():
+    words = fetch_words()
+    print(words)
+
+
 if __name__ == '__main__':
-    fetch_words()
+    main()
 
 # to execute this, python words.py in REPL after doing import words
 
