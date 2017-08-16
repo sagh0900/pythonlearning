a9cd6d2467e9374ca41356376b4d4699b060cb3d
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-16 12:11:33 +0200

    comments and docstrings to the module

--- pyfund/words.py
+++ pyfund/words.py
@@ -1,8 +1,29 @@
+#!/usr/bin/env python3
+"""Retrieve and print words from a URL.
+
+Usage:
+
+     python words.py <URL>
+
+"""
+# The above is a module docstring
 import sys
 from urllib.request import urlopen
+"""This is called docstring"""
 
 
 def fetch_words(url):
+    """"Fetch a list of words from URL
+    Args:
+        url: The URL of a UTF-8 text document
+        
+    Return:
+        A list of strings containing the words from document
+        
+    """
+    # The above docstring is function docstring & can be accessed from REPL by
+    # from words import *
+    # help(fetch_words)
     with urlopen('http://sixty-north.com/c/t.txt') as story:
             story_words = []
             for line in story:
@@ -13,6 +34,12 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Print items one per line
+    
+    Args:
+        An iterable series of printable items.
+    
+    """
     for item in items:
         print(item)
 
@@ -20,12 +47,17 @@ def print_items(items):
 
 
 def main(url):
+    """Print each word from a text document from a URL
+    
+    Args:
+        URL: The URL of a UTF-8 text document. 
+    """
     words = fetch_words(url)
     print_items(words)
 
 
 if __name__ == '__main__':
-    main(sys.argsv[1])
+    main(sys.argv[1])  # The 0th of the argument is name of the module file itself.
 
 # to execute multiple functions in a module, in REPL, from words import (fetch_words, print_words)
 # print_words(fetch_words())
