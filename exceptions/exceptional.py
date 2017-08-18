b1bd92f17bdba0624d66e846d63d1e036291f1f3
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-18 10:44:13 +0200

    using raise keyword in exceptions

--- exceptions/exceptional.py
+++ exceptions/exceptional.py
@@ -1,15 +1,25 @@
 ''' A module for demonstrating exceptions '''
+import sys
+from math import log
 
 
 def convert(s):
-    ''' convert to an integer. '''
+    """converting integer literal in string representation to int
+     convert to an integer."""
     try:
         x = int(s)
         print("conversion succeeded! x =", x)
-    except (ValueError, TypeError):
-        #  print("conversion failed!")
-        pass
-    return x
+        return x
+    except (ValueError, TypeError) as e:
+        print("conversion error: {}"
+              .format(str(e)),
+              file=sys.stderr)
+        raise
+
+
+def string_log(s):
+    v = convert(s)
+    return log(v)
 
 
 # if string is passed which is not convertable will throw ValueError exception.
