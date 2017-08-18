c4717e92947acb6cb2edaacd00df1d33b172aea4
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-18 10:11:20 +0200

    exception module better way

--- exceptions/exceptional.py
+++ exceptions/exceptional.py
@@ -3,7 +3,14 @@
 
 def convert(s):
     ''' convert to an integer. '''
-    x = int(s)
+    try:
+        x = int(s)
+        print("conversion succeeded! x =", x)
+    except ValueError:
+        print("conevsrion failed")
+        x = -1
+    except TypeError:
+        print("cannot convert collection object, x =", s)
     return x
 
 
