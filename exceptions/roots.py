3dae9278ddc08f136b715d1e86e01d69a0663c82
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-18 10:58:14 +0200

    Introducing ZeroDivisonError Exception

---
+++ exceptions/roots.py
@@ -0,0 +1,33 @@
+def sqrt(x):
+    """ Compute square roots using method of heron of Alexandria.
+    
+    Args:
+        x:  The number for which square root is to be computed
+        
+    Returns:
+        The square root of x
+    """
+    guess = x
+    i = 0
+    while guess * guess != x and i < 20:
+        guess = (guess + x / guess) / 2.0
+        i += 1
+    return guess
+
+
+def main():
+    print(sqrt(9))
+    print(sqrt(2))
+    try:
+        print(sqrt(-1))  # will raise ZeroDivisionError exception
+    except ZeroDivisionError:
+        print("Cannot compute a square root of a negative number")
+
+    print("Program Execution continues normally")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
