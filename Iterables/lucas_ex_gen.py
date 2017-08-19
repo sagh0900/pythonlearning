794c0b9ff870ba067473fe8a9f6c750be20be1eb
sagh0900 <ghanta.saikrishna@gmail.com>
2017-08-19 15:37:20 +0200

    done with iterables

--- Iterables/lucas_ex_gen.py
+++ Iterables/lucas_ex_gen.py
@@ -1,4 +1,14 @@
 #  prints values till computer runs out of memory, forced termination needed.
+from math import sqrt
+
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+        return True
 
 
 def lucas():
@@ -9,6 +19,9 @@ def lucas():
         yield b
         a, b = b, a+ b
 
+if __name__ == '__main__':
+    obj = []
+    for x in (p for p in lucas() if is_prime(p)):
+        obj.append(x)
 
-for x in lucas():
-    print(x)
+    print(obj)
--- Iterables/zipper.py
+++ Iterables/zipper.py
@@ -1,4 +1,7 @@
-sunday = ['atheism', 'theist', 'consciousness', 'spirituality', 'god']
-monday = ['fact', 'bullshit', 'yoga', 'agnoism', 'crap']
-for item in zip(ghanta, others):
+sunday = [10, 22, 34, 51]
+monday = [22, 35, 41, 47]
+for item in zip(sunday, monday):
     print(item)
+for sun, mon in zip(sunday, monday):
+    print("average =", (sun + mon) / 2)
+
